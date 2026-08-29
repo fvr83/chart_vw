4067d971b365214349e12f0d675c18cf2696ec56
Felipe <felipe@mail>
2026-08-28 23:56:54 -0300

    8 added

--- draw_charts.py
+++ draw_charts.py
@@ -479,7 +479,7 @@ for path in Path(origin_folder).iterdir():
     file_name = file_extension.removesuffix(".txt")
     gap, tier, depth, game_mode, chip_mode = file_name.split("_") # final_folder
 
-    if depth not in ["9bb"]: 
+    if depth not in ["8bb"]: 
 
         continue
     content = path.read_text(encoding="utf-8")
